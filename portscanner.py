3f35d5f8b48ae3b6a9db4fb95c74e5df10974b6a
Raghav Maheshwari <75838458+raghvmaheshwari@users.noreply.github.com>
2023-02-06 02:45:37 +0530

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -1,6 +1,14 @@
 import socket  # used to establish connection
 from IPy import IP  # used to convert domain name to ip address
 
+def scan(target,port_start,port_end):
+    #storing the input in another variable after conversion
+    converted_ip = check_ip(target)
+    print ('\n' + 'Scanning ' + str(target))
+    # Loop To check for every port number
+    for port in range(port_start,port_end):
+        scan_port(converted_ip, port)
+
 #Function to check if input is domain name or ip address
 def check_ip(ip):
     try:
@@ -10,7 +18,8 @@ def check_ip(ip):
     #If user has given domain name it will give value error
     except ValueError:
         return socket.gethostbyname(ip)
-
+def get_banner(s):
+    return s.recv(1024)
 
 # Scan Port function
 def scan_port(ipaddress, port):
@@ -19,25 +28,23 @@ def scan_port(ipaddress, port):
         # this is to make portscanner work faster. For higher the time of timeout the greater the accuracy of our portscanneer
         sock.settimeout(0.5)
         sock.connect((ipaddress, port))
-
-        # using str function because value of port is in integer.
-        print('!!Port ' + str(port) + ' is OPEN!!')
+        try:
+            banner =  get_banner(sock)
+            # using str function because value of port is in integer.
+            print('Open Port ' + str(port) + ':' + str(banner.decode().stripe('\n')))
+        except:
+            print('Open Port ' + str(port) )
     except:
+        #not intrested in which ports are closed so pass
         pass
 
-def scan(target):
-    #storing the input in another variable after conversion
-    converted_ip = check_ip(target)
-    print ('\n' + 'Scanning ' + str(target))
-    # Loop To check for every port number
-    for port in range(75, 85):
-        scan_port(converted_ip, port)
-        
 #input from user:
 target = input('[+] Enter The Target/s That You Want To Scan: (Split the multiple targets with ,)')
+port_start = int(input('Enter Starting Port Number:'))
+port_end = int(input('Enter Ending Port Number:'))
 if ',' in target:
     for ip_add in target.split(','):
-        scan(ip_add.strip(' '))
+        scan(ip_add.strip(' '),port_start,port_end)
 else:
-    scan(target)
+    scan(target,port_start,port_end)
 
